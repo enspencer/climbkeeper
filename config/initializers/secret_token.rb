99c4824a75a74597a85aa00996278b5162daba37
Matt Clement <darkmoves@gmail.com>
2013-10-21 18:59:26 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Climbkeeper::Application.config.secret_key_base = '53355e4dc969ff2d2b136b2057775f3a6befbb40c8b742328e4d99105b1267261ed360f4a8735888314c4e708b0f9fcfe89ea96fd44cb2330dcf938f0fcd50ec'
