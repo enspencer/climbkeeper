50afb5b2c2760d7cf578b22e8c5b4182552c64ee
Matt Clement <darkmoves@gmail.com>
2013-10-21 20:48:44 -0400

    Added a home_controller.rb file and created the index method for the controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Climbkeeper::Application.routes.draw do
   devise_for :climbers
 
-  root "home#index"
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
