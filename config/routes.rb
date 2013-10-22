5b85e1b9e596215f2a56a63504a85fc3bc97bc45
Matt Clement <darkmoves@gmail.com>
2013-10-21 20:20:21 -0400

    Generated a climber model using devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Climbkeeper::Application.routes.draw do
+  devise_for :climbers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
