cb9b173b5152b7f1ad8a72b1adec7c98b125e70e
Matt Clement <darkmoves@gmail.com>
2013-10-23 16:09:33 -0400

    Added relationships to the rockgym and review models.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ActiveRecord::Base
-
+	belongs_to :problem
+	belongs_to :climber
 end
--- app/models/rockgym.rb
+++ app/models/rockgym.rb
@@ -1,3 +1,4 @@
 class Rockgym < ActiveRecord::Base
-
+	has_many :problems
+	has_many :reviews, through: :problems
 end
