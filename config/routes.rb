ee0613d7cdf77505fe01d2f36e7509da9bcb2f2c
Matt Clement <darkmoves@gmail.com>
2013-10-23 17:01:28 -0400

    Changed root to point to rockgyms#index.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Climbkeeper::Application.routes.draw do
     resources :reviews
   end
 
-#  root 'home#index'
+  root 'rockgyms#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
