73e77560be6fcd7aae542ff4ed9de415db80a09b
Matt Clement <darkmoves@gmail.com>
2013-10-25 17:01:09 -0400

    Edited the Rockgym controller, index view and show view to handle the case of no Rockgyms without freaking the fuck out.

--- app/controllers/rockgyms_controller.rb
+++ app/controllers/rockgyms_controller.rb
@@ -1,13 +1,16 @@
 class RockgymsController < ApplicationController
 
 	def index
-		@rockgyms = Rockgym.all
-		@reviews = Review.order("created_at DESC").limit(8)
+			@rockgyms = Rockgym.all
+			@reviews = Review.order("created_at DESC").limit(8)
 	end
 
 	def show
-		@rockgym = Rockgym.find(params[:id])
-		@problem = Problem.new
+		if Rockgym.all != []
+			@rockgym = Rockgym.find(params[:id])
+			@problem = Problem.new
+		else
+		end
 	end
 
 	def new
