de78b0594713c01720f754309dda70bed578c815
Matt Clement <darkmoves@gmail.com>
2013-10-26 18:42:45 -0400

    Added has_attached_file code to Problems model.

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,4 +2,6 @@ class Problem < ActiveRecord::Base
 	has_many :reviews
 	belongs_to :rockgym
 	belongs_to :climber
+
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 end
