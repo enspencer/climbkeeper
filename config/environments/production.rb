d3e8d9ef02d45185fa09ed0f398263debc5a7d96
Matt Clement <darkmoves@gmail.com>
2013-10-26 20:07:49 -0400

    Added missed bracket in production.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,12 @@ Climbkeeper::Application.configure do
 
   # Configure Paperclip defaults to use AWS S3 as default storage.
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
 
   # Code is not reloaded between requests.
