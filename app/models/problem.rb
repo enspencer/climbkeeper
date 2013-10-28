e904cdf8fef63445187584ad045ae2d0a14fbd40
Matt Clement <darkmoves@gmail.com>
2013-10-28 13:53:35 -0400

    Preliminary code to create a word frequency array for problem reviews.

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -4,4 +4,34 @@ class Problem < ActiveRecord::Base
 	belongs_to :climber
 
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
+	# Cut and paste to start a function to gather word frequency for in all problem reviews.
+
+	# def review_words
+	# 	words = []
+	# 	obama_tweets.each do |item|
+	# 		words << item[:tweet].split(" ")
+	# 	end
+
+	# 	#binding.pry
+
+	# 	words.flatten!
+
+	# 	binding.pry
+
+	# 	freq = {}
+
+	# 	words.each do |word|
+	# 		freq[word.to_sym] ||= 0 # =>
+	# 		# The above is the same thing as the following two lines of code. 
+	# 		# if freq[word.to_sym]
+	# 		# 	freq[word.to_sym] = 0
+	# 		freq[word.to_sym] += 1
+	# 	end
+
+	# 	binding.pry
+
+	# 	freq.sort {|_key, value| value}
+	# end
+
 end
